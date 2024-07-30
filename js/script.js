057dcedae567f0630d9db6f22f33c2130d4692df
Abhinand S <abhinandappu2005@gmail.com>
2024-07-30 22:13:18 +0530

    feat: Update HTML, CSS, and JS for improved layout and functionality
    - Enhanced HTML structure with new sections and elements
    - Updated CSS styles for better responsiveness and design consistency
    - Refactored JavaScript for improved performance and added new features

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let nav = document.querySelector(".nav-wrap");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("scroll-on");
+    } else {
+        nav.classList.remove("scroll-on");
+    }
+}
+
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
+        navCollapse.classList.remove("show");
+    })
+})
+
+$(document).ready(function () {
+    $('a[href^="#"]').on('click', function (event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top - $('.nav-wrap').outerHeight()
+            }, 100);
+        }
+    });
+});
